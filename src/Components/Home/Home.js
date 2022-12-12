3c25a9cf475e88339df503bf50d177079680545c
Juhani Koski <juhkukoskii@gmail.com>
2022-12-12 14:37:47 +0200

    v3

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,40 +1,13 @@
+import { Link } from 'react-router-dom'
 import HomeCSS from './Home.module.css'
 
 export default function Home() {
   return (
     <div className={HomeCSS.container}>
-        <article className={HomeCSS.containerItem}>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Dicta qui a laudantium exercitationem repudiandae dolore
-            voluptate nobis recusandae? Unde repudiandae dolores nemo
-            saepe fugit aliquam nihil voluptatem quaerat voluptate sint.
-          </p>
-        </article>
-        <article className={HomeCSS.containerItem}>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Dicta qui a laudantium exercitationem repudiandae dolore
-            voluptate nobis recusandae? Unde repudiandae dolores nemo
-            saepe fugit aliquam nihil voluptatem quaerat voluptate sint.
-          </p>
-        </article>
-        <article className={HomeCSS.containerItem}>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Dicta qui a laudantium exercitationem repudiandae dolore
-            voluptate nobis recusandae? Unde repudiandae dolores nemo
-            saepe fugit aliquam nihil voluptatem quaerat voluptate sint.
-          </p>
-        </article>
-        <article className={HomeCSS.containerItem}>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Dicta qui a laudantium exercitationem repudiandae dolore
-            voluptate nobis recusandae? Unde repudiandae dolores nemo
-            saepe fugit aliquam nihil voluptatem quaerat voluptate sint.
-          </p>
-        </article>
+      <div className={HomeCSS.containerItem}>
+        <h1>Welcome</h1>
+        <h3><Link to="/v1" >visualizations here</Link></h3>
       </div>
+    </div>
   )
 }
